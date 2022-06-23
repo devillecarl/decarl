bfaacecf4229205d52d236fee2c7c49296bb44e3
decarl <devillecarl@gmail.com>
2022-06-23 16:43:14 -0600

    commit on master

--- pset1a.py
+++ pset1a.py
@@ -1,4 +1,4 @@
-#MITx 6.00.1x pset1
+#MITx 6.00.1x pset1 for june 2022
 import time # for timing
 #Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
 #The following variables contain values as described below:
